600aab3a5269289f8cd33854a003d4138c83e065
Thanh Le <lenguyenthanh@hotmail.com>
2019-04-08 18:34:44 +0200

    Remove temporary file

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 curl -LJO https://github.com/lenguyenthanh/dotfiles/archive/master.zip
 unzip -a dotfiles-master.zip
 mv -f dotfiles-master .dotfiles
+rm dotfile-master.zip
 cd .dotfiles
 ./bootstrap.sh
