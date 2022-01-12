ab16679a8a47ef12f0dfc446378f72b3d1046b58
Thanh Le <lenguyenthanh@hotmail.com>
2022-01-12 18:29:54 +0100

    Use jdk 17

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -2,16 +2,16 @@
 export PATH="~/.local/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin:$PATH"
 
 # JAVA
-export JAVA_16_HOME=$(/usr/libexec/java_home -v16)
-export JAVA_14_HOME=$(/usr/libexec/java_home -v14)
-export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
+#export JAVA_16_HOME=$(/usr/libexec/java_home -v16)
+#export JAVA_14_HOME=$(/usr/libexec/java_home -v14)
+#export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
 
 #export JAVA_HOME="$(/usr/libexec/java_home -v1.8)"
-export JAVA_HOME="$(/usr/libexec/java_home -v16)"
+export JAVA_HOME="$(/usr/libexec/java_home -v17)"
 
-alias java16='export JAVA_HOME=$JAVA_16_HOME'
-alias java14='export JAVA_HOME=$JAVA_14_HOME'
-alias java8='export JAVA_HOME=$JAVA_8_HOME'
+#alias java16='export JAVA_HOME=$JAVA_16_HOME'
+#alias java14='export JAVA_HOME=$JAVA_14_HOME'
+#alias java8='export JAVA_HOME=$JAVA_8_HOME'
 
 export ANDROID_HOME="/usr/local/share/android-sdk"
 export ANDROID_SDK_ROOT="/usr/local/share/android-sdk"
