1b2a9ce500aef9e15ab251471a32d2969a7edb05
Thanh Le <lenguyenthanh@hotmail.com>
2023-07-11 21:09:26 +0200

    Backup at Tue Jul 11 21:09:26 CEST 2023

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,11 +1,6 @@
 # PATH
 PATH="/opt/homebrew/bin:/opt/homebrew/sbin:~/.local/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin:$PATH"
 
-export JAVA_HOME="$(/usr/libexec/java_home -v 17)"
-
-# JAVA
-PATH="$JAVA_HOME/bin:$PATH"
-
 # RUST
 PATH="$HOME/.cargo/bin:$PATH"
 export RUST_SRC_PATH=$(rustc --print sysroot)/lib/rustlib/src/rust/src/
@@ -13,11 +8,14 @@ export RUST_SRC_PATH=$(rustc --print sysroot)/lib/rustlib/src/rust/src/
 # mactext
 PATH=$PATH:/Library/TeX/texbin/
 
-export RUBY_HOME="/opt/homebrew/lib/ruby/gems/3.2.0/bin:/opt/homebrew/opt/ruby/bin"
-
 # jenv
-export PATH="$RUBY_HOME:$HOME/.jenv/bin:$PATH:$HOME/.flutter/bin"
+PATH="$HOME/.jenv/bin:$PATH"
 eval "$(jenv init -)"
+
+export JAVA_HOME="$(/usr/libexec/java_home -v 20)"
+
+# JAVA
+export PATH="$JAVA_HOME/bin:$PATH"
 
 # private local path
 . $DOTFILES/zsh/path.local
